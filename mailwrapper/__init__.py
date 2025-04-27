32126747bbb3b39c0835d6256acdd2865bf12602
trojvn <mrx.trojan@gmail.com>
2025-04-27 16:04:17 +0300

    refactor

--- mailwrapper/__init__.py
+++ mailwrapper/__init__.py
@@ -1,4 +1,6 @@
 from .anymessage import AnyMessage
 from .bower import Bower
+from .models.anymessage import AnyMessageResponse
+from .models.bower import BowerResponse
 
-__all__ = ["AnyMessage", "Bower"]
+__all__ = ["AnyMessage", "Bower", "AnyMessageResponse", "BowerResponse"]
