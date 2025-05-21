c91d5867e5702de158abbdffcec193c410018bb3
trojvn <mrx.trojan@gmail.com>
2025-05-21 15:20:26 +0300

    fixed

--- mailwrapper/aliases.py
+++
@@ -1,7 +0,0 @@
-from .anymessage import AnyMessage
-from .bower import Bower
-from .models.anymessage import AnyMessageResponse
-from .models.bower import BowerResponse
-
-EmailService = AnyMessage | Bower
-EmailResponses = AnyMessageResponse | BowerResponse
--- mailwrapper/models/__init__.py
+++ mailwrapper/models/__init__.py
@@ -1,9 +1,1 @@
-from dataclasses import dataclass
 
-from mailwrapper.aliases import EmailResponses, EmailService
-
-
-@dataclass
-class EmailResponse:
-    instance: EmailService
-    response: EmailResponses
