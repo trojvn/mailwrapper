b5a820127f2be4ffe0820c5b73084bfe21df1ea3
trojvn <mrx.trojan@gmail.com>
2025-05-29 14:50:05 +0300

    fixed

--- mailwrapper/models/options.py
+++ mailwrapper/models/options.py
@@ -5,14 +5,20 @@ from dataclasses import dataclass
 class EmailOptions:
     anymessage_service: str
     bower_service: str
+    yuharan_service: str
+
     anymessage_domain: str = ""
     bower_domain: str = ""
+    yuharan_domain: str = ""
 
     anymessage_token: str = ""
     bower_token: str = ""
+    yuharan_token: str = ""
 
     def __post_init__(self):
         if not self.anymessage_domain:
             self.anymessage_domain = "rambler,hotmail,outlook,gmail"
         if not self.bower_domain:
             self.bower_domain = "gmail.com"
+        if not self.yuharan_domain:
+            self.yuharan_domain = "rambler,hotmail,outlook,gmail"
