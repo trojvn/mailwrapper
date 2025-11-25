985916b54e339467e09c20af96be3f5c707a5fd3
trojvn <mrx.trojan@gmail.com>
2025-11-25 16:04:55 +0300

    fix addr

--- mailwrapper/async_yuharan.py
+++ mailwrapper/async_yuharan.py
@@ -3,4 +3,4 @@ from mailwrapper.base import BaseAnyMessage
 
 class AsyncYuharan(BaseAnyMessage):
     def __init__(self, token: str, proxy: str | None = None):
-        super().__init__("https://inboxifyapi.yuharan.ru", token, proxy)
+        super().__init__("http://yuharan.ru:4545", token, proxy)
